316a577ae82a77cb79fc3dcb41f2c958c7d7880c
Dmitriy Voronin <dvoronin@avito.ru>
2015-04-22 16:54:10 +0300

    trying more verbose output

--- validate.js
+++ validate.js
@@ -1,8 +1,8 @@
-var validator = require('is-my-json-valid/require');
-
-var validate = validator('pt_realty_form_schema.json');
+var validator = require('is-my-json-valid');
+var schema = require('./pt_realty_form_schema.json');
+var validate = validator(JSON.parse(schema), {verbose: true}, {greedy: true});
 
 var result = validate('protools_form_full.json');
-if(!result) {
-throw JSON.stringify(validate.errors);
+if (!result) {
+    throw JSON.stringify(validate.errors);
 }
