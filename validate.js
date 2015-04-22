9ba1342146b004781f25c6477e4bba86ca868083
Dmitriy Voronin <dvoronin@avito.ru>
2015-04-22 17:09:16 +0300

    verbose output

--- validate.js
+++ validate.js
@@ -1,8 +1,12 @@
 var validator = require('is-my-json-valid');
-var schema = require('./pt_realty_form_schema.json');
-var validate = validator(JSON.parse(schema), {verbose: true}, {greedy: true});
+var fs = require('fs');
+var schema = JSON.parse(fs.readFileSync('./pt_realty_form_schema.json', 'utf8'));
+var target = JSON.parse(fs.readFileSync('./protools_form_full.json', 'utf8'));
+var validate = validator(schema, {verbose: true, greedy: true});
 
-var result = validate('protools_form_full.json');
+var result = validate(target);
 if (!result) {
-    throw JSON.stringify(validate.errors);
+    throw validate.errors.map(function (obj) {
+        return JSON.stringify(obj);
+    }).join("\n");
 }
