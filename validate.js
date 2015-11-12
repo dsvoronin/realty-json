360f0be8939e18ba77cdfc920ab04e59e228de03
Mikhail Asavkin <limenutt@gmail.com>
2015-11-12 14:30:32 +0300

    support several forms validation

--- validate.js
+++ validate.js
@@ -1,12 +1,33 @@
 var validator = require('is-my-json-valid');
 var fs = require('fs');
-var schema = JSON.parse(fs.readFileSync('./pt_realty_form_schema.json', 'utf8'));
-var target = JSON.parse(fs.readFileSync('./protools_form_full.json', 'utf8'));
-var validate = validator(schema, {verbose: true, greedy: true});
+var tests = [
+    {
+        schema: './pt_form_schema.json',
+        targets: [
+        './pt_realty_form.json',
+        './pt_auto_form.json',
+        ]
+    }
+    // ,
+    // {
+    //     schema: './pt_services_form_schema.json',
+    //     targets: [
+    //     './services_form_moving.json'
+    //     ]
+    // }
+];
 
-var result = validate(target);
-if (!result) {
-    throw validate.errors.map(function (obj) {
-        return JSON.stringify(obj);
-    }).join("\n");
-}
+tests.forEach(function(test) {
+   var schema = JSON.parse(fs.readFileSync(test.schema, 'utf8'));
+   test.targets.forEach(function(targetPath) {
+      var target = JSON.parse(fs.readFileSync(targetPath, 'utf8'));
+      var validate = validator(schema, {verbose: true, greedy: true});
+      console.log('validating [' + targetPath + '] against [' + test.schema + ']...');
+      var result = validate(target);
+      if (!result) {
+        throw validate.errors.map(function (obj) {
+            return JSON.stringify(obj);
+        }).join('\n');
+      }
+    });
+});
